356abd88942e0532ea8093b04d0c19254decf3b8
Alegre <115659045+Alegree@users.noreply.github.com>
2023-11-06 18:12:55 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -6,10 +6,10 @@ function checkNumber() {
         const number = parseInt(numberInput.value);
 
         if (number % 2 === 0 && number > 0) {
-            document.body.style.backgroundColor = 'blue';
+            document.body.style.backgroundColor = 'rgb(0,181,226)';
             resultDiv.textContent = `O número ${number} é PAR!`;
         } else if (number % 2 !== 0 && number >= 0) {
-            document.body.style.backgroundColor = 'orange';
+            document.body.style.backgroundColor = 'rgb(250,198,104)';
             resultDiv.textContent = `O número ${number} é ÍMPAR!`;
         } else {
             alert('Apenas números positivos são permitidos.');
